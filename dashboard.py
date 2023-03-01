ba84adc0fc43cc02a34e483b27cea9c1dc4738f0
Valériane Evin <121805896+ValerianeE@users.noreply.github.com>
2023-03-01 15:14:48 +0100

    Changement dashboard, ajout X_test_sampled

--- dashboard.py
+++ dashboard.py
@@ -3,8 +3,10 @@ import pandas as pd
 import seaborn as sns
 import requests
 import shap
+import pickle
 from shap.plots import waterfall
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 #Configurations
 st.set_page_config( 
@@ -13,14 +15,23 @@ st.set_page_config(
 )
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
+#Import données pour les shap values
+X_test_sampled = pd.read_csv('X_test_sampled.csv',delimiter= ',')
+
+# Import du modèle
+model = pickle.load(open('model.pkl','rb'))
+
+#Calcul des Shap Values
+shap_values = shap.TreeExplainer(model).shap_values(X_test_sampled)
+
 ##############################
 # Fonctions
 ##############################
 
 # Shap summary global
-#@st.cache
-#def shap_summary(shap_vals,features):
-#    return shap.summary_plot(shap_vals, feature_names=features)
+@st.cache
+def shap_summary(shap_vals,features,num_features):
+    return shap.summary_plot(shap_vals, features,max_display=num_features)
 
 # Graphique shap local
 @st.cache
@@ -85,15 +96,15 @@ graphique = pd.read_csv('graphique.csv',delimiter= ',')
 graphique.set_index('SK_ID_CURR',inplace=True)
 option2 = st.sidebar.selectbox(
     'Quelle première variable souhaitez-vous analyser par rapport au demandeur?',
-    graphique.columns)
+    graphique.iloc[:, 0:10].columns)
 st.sidebar.write('Vous avez choisi:', option2)
 
 option3 = st.sidebar.selectbox(
     'Quelle seconde variable souhaitez-vous analyser par rapport au demandeur?',
-    graphique.columns)
+    graphique.iloc[:, 0:10].columns)
 st.sidebar.write('Vous avez choisi:', option3)
 
-#Liste des features quantitatives ou discrètes:
+#Liste des variables qualitatives:
 quali=["CNT_FAM_MEMBERS","REGION_RATING_CLIENT","LIVE_REGION_NOT_WORK_REGION","OBS_30_CNT_SOCIAL_CIRCLE","DEF_30_CNT_SOCIAL_CIRCLE","FLAG_DOCUMENT_9","FLAG_DOCUMENT_17"]
 
 ##############################
@@ -114,29 +125,63 @@ if btn_predict:
     else: 
         st.success("C'est bon! Le demandeur a une grande probabilité de rembourser son prêt!")
     
+    ###############
     # Graphiques
+    ###############
+
+    #Jauge
+    fig = go.Figure(go.Indicator(
+    mode = "gauge+number+delta",
+    value = proba,
+    domain = {'x': [0, 1], 'y': [0, 1]},
+    title = {'text': "Jauge du taux de probabilité de remboursement", 'font': {'size': 30}},
+    delta = {'reference': 0.5, 'increasing': {'color': "RebeccaPurple"}},
+    gauge = {
+        'axis': {'range': [None, 1], 'tickwidth': 1, 'tickcolor': "darkblue"},
+        'bar': {'color': "black"},
+        'bgcolor': "white",
+        'borderwidth': 2,
+        'bordercolor': "gray",
+        'steps': [
+            {'range': [0, 0.5], 'color': 'crimson'},
+            {'range': [0.5, 1], 'color': 'springgreen'}],
+        'threshold': {
+            'line': {'color': "black", 'width': 4},
+            'thickness': 0.75,
+            'value': 0.5}}))
+    fig.update_layout(paper_bgcolor = "lavender", font = {'color': "darkblue", 'family': "Arial"})
+    st.write(fig)
+
+    #Waterfall plot
     X_train = pd.read_csv('colonnes.csv',delimiter= ',')
     st.markdown('SHAP waterfall Plot pour le client demandeur de prêt:')
     waterfall_plot(10, X_train.columns, d[2], pd.DataFrame.from_dict(d[1]["0"],orient='index').values[:,0])    
     plt.gcf()
     st.pyplot(plt.gcf())
 
-    #st.markdown('SHAP Summary Plot pour le client demandeur de prêt:')
-    #shap_summary(json.dumps(d[3]),X_train.columns)    
-    #plt.gcf()
-    #st.pyplot(plt.gcf())
+    #Summary plot
+    st.markdown('SHAP Summary Plot pour le client demandeur de prêt:')
+    shap_summary(shap_values,X_test_sampled,7)    
+    plt.gcf()
+    st.pyplot(plt.gcf())
 
+    #Kdeplot ou diagramme en barres
     if option2 in quali:
         graphique[option2] = graphique[option2].astype(int)
         st.markdown('**Diagramme en barres de** ' + str(option2) + " **:**")
         fig = plt.figure()
-        palette=['grey' if (x != int(graphique[option2].loc[[option1]].values)) else 'red' for x in graphique[option2].value_counts().index.sort_values()]
+        palette=['#c5c9c7' if (x != int(graphique[option2].loc[[option1]].values)) else '#840000' for x in graphique[option2].value_counts().index.sort_values()]
         sns.countplot(x=graphique[option2], palette=palette)
         st.pyplot(fig.figure)
     else:
         st.markdown('**Kdeplot de** ' + str(option2)+ " **:**")
         fig = plt.figure()
-        fig = sns.kdeplot(data=graphique, x=option2)
+        fig = sns.kdeplot(data=graphique, x=option2,hue="Catégories de client", cut=0, fill=True, linestyle="--")
+        lss = ['--', ':']
+        handles = fig.legend_.legendHandles[::-1]
+        for line, ls, handle in zip(fig.collections, lss, handles):
+            line.set_linestyle(ls)
+            handle.set_ls(ls)
         plt.axvline(int(graphique[option2].loc[[option1]].values),color='r')
         st.pyplot(fig.figure)
     st.write('Voici la valeur du demandeur de prêt par rapport aux données', int(graphique[option2].loc[[option1]].values))
@@ -145,16 +190,41 @@ if btn_predict:
         graphique[option3] = graphique[option3].astype(int)
         st.markdown('**Diagramme en barres de** ' + str(option3)+ " **:**")
         fig = plt.figure()
-        palette=['grey' if (x != int(graphique[option3].loc[[option1]].values)) else 'red' for x in graphique[option3].value_counts().index.sort_values()]
+        palette=['#c5c9c7' if (x != int(graphique[option3].loc[[option1]].values)) else '#840000' for x in graphique[option3].value_counts().index.sort_values()]
         sns.countplot(x=graphique[option3], palette=palette)
         st.pyplot(fig.figure)
     else:
         st.markdown('**Kdeplot de** ' + str(option3)+ " **:**")
         fig = plt.figure()
-        fig = sns.kdeplot(data=graphique, x=option3)
+        fig = sns.kdeplot(data=graphique, x=option3,hue="Catégories de client", cut=0, fill=True, linestyle="--")
+        lss = ['--', ':']
+        handles = fig.legend_.legendHandles[::-1]
+        for line, ls, handle in zip(fig.collections, lss, handles):
+            line.set_linestyle(ls)
+            handle.set_ls(ls)
         plt.axvline(int(graphique[option3].loc[[option1]].values),color='r')
         st.pyplot(fig.figure)
     st.write('Voici la valeur du demandeur de prêt par rapport aux données', int(graphique[option3].loc[[option1]].values))
-    
 
-
+    # Scatterplot dans le cas de deux variables quantitatives:
+    if option2 not in quali and option3 not in quali:
+        st.markdown('**Nuage de points de** ' + str(option2)+ " **et de** "+ str(option3) + " **:**")
+        fig = plt.figure()
+        sns.scatterplot(x=option2,y=option3, data=graphique, hue="Score", palette ="flare")
+        st.pyplot(fig.figure)
+    #Tableau de contingence si les deux variables sont qualitatives
+    elif option2 in quali and option3 in quali:
+        st.markdown('**Tableau de contingence de** ' + str(option2)+ " **et de** "+ str(option3) + " **:**")
+        data_crosstab = pd.crosstab(graphique[option2], 
+            graphique[option3],
+            margins = False)
+        st.table(data_crosstab)
+    #Boxplot si une variable est qualitatives et l'autre quantitatives
+    else:
+        st.markdown('**Boxplot de** ' + str(option2)+ " **et de** "+ str(option3) + " **:**")
+        fig = plt.figure()
+        if option2 in quali:
+            sns.boxplot(x=option2,y=option3, data=graphique)
+        else:
+            sns.boxplot(x=option3,y=option2, data=graphique)
+        st.pyplot(fig.figure)
